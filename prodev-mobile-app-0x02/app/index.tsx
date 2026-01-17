920c97800890f99a6cc78a55d4ffcb8c27514032
GH-Tyron <110431714+GH-Tyron@users.noreply.github.com>
2026-01-17 22:06:53 +0000

    Update index.tsx

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -26,16 +26,11 @@ export default function Index() {
 
             {/* Text Group */}
             <View style={styles.textGroup}>
-              <Text style={styles.textLarge}>
-                Find your favorite place here
-              </Text>
-              <Text style={styles.textSmall}>
-                The best prices for over 2
-              </Text>
-              <Text style={styles.textSmall}>
-                million properties worldwide
-              </Text>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2 </Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
             </View>
+
 
             {/* Buttons + Navigation Prompt */}
             <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
